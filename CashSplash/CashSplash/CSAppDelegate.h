b0471b3f17e43189bdc9fd703fe4c5b8ea2bc690
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2014-03-15 13:28:06 +0100

    Added a new Empty App project.

---
+++ CashSplash/CashSplash/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  CashSplash
+//
+//  Created by Ivan Fabijanovic on 15/03/14.
+//  Copyright (c) 2014 Ivan Fabijanovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
