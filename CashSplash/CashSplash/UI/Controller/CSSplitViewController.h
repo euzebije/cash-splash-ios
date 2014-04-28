b5f8d82696fc5097ecb6a2dcdb0d34cd6b8b5e64
Ivan Fabijanovic <feb@shoutem.com>
2014-04-28 21:24:29 +0200

    iPad menu icons and polish.

---
+++ CashSplash/CashSplash/UI/Controller/CSSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSSplitViewController.h
+//  CashSplash
+//
+//  Created by Ivan Fabijanovic on 28/04/14.
+//  Copyright (c) 2014 Ivan Fabijanovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSSplitViewController : UISplitViewController <UISplitViewControllerDelegate>
+
+@end
